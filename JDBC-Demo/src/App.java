811340d7d3659a3ade93b7aac77b45f0342bdfb6
Namdev Panchal <144767447+namdev0206@users.noreply.github.com>
2025-09-23 12:32:37 +0530

    Add files via upload

---
+++ JDBC-Demo/src/App.java
@@ -0,0 +1,32 @@
+import java.sql.*;
+
+public class App 
+{
+	public static void main(String args[])
+	{
+		try
+		{
+			//Statement to load driver class
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Driver Class Loaded");
+			
+			//Statement to create connection statement
+			Connection cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc10", "root", "Nik@2001");
+			System.out.println("Connection Object Created");
+			
+			Statement st = cn.createStatement();
+			System.out.println("Selection Object is Created");
+			
+			st.execute("create table productInfo(pid int primary key, name varchar(20), price int)");
+			System.out.println("SQL Query is Executed");
+			
+			cn.close();
+			
+			System.out.println("Connection is Closed");;
+		}
+		catch(Exception ex)
+		{
+			System.out.println(ex);
+		}
+	}
+}
