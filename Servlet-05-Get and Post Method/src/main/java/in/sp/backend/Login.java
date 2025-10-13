9a774ef92c33346ad52255adc8c535b1aaf342af
Namdev Panchal <144767447+namdev0206@users.noreply.github.com>
2025-10-13 12:49:30 +0530

    Add files via upload

---
+++ Servlet-05-Get and Post Method/src/main/java/in/sp/backend/Login.java
@@ -0,0 +1,29 @@
+package in.sp.backend;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/mylogin")
+public class Login extends HttpServlet
+{
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		String myemail = (String) request.getParameter("name1");
+		String mypass = (String) request.getParameter("pass1");
+		
+		if(myemail.equals("deepak@gmail.com") && mypass.equals("deepak123"))
+		{
+			System.out.println("Success");
+		}
+		else
+		{
+			System.out.println("Failed");
+		}
+	}
+}
